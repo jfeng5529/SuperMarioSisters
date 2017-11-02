c6a6e2ec167c6bc94a33292c14dfa49d2ca0f830
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-11-02 11:37:09 -0400

    create new practice class

---
+++ SuperMarioSisters/src/caveExplore/TreasureRoom.java
@@ -0,0 +1,10 @@
+package caveExplore;
+
+public class TreasureRoom extends NPCRoom {
+
+	public TreasureRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
