e5f950df6bdb4a362efe8efc2b3c8409c4ecbc63
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-11-06 11:14:58 -0500

    changes to mimi's room

--- SuperMarioSisters/src/jessiMimiGame/MimiRoom.java
+++ SuperMarioSisters/src/jessiMimiGame/MimiRoom.java
@@ -2,19 +2,63 @@ package jessiMimiGame;
 
 import java.util.Random;
 import caveExplore.CaveExplorer;
+import caveExplore.NPC;
 import caveExplore.NPCRoom;
 
 public class MimiRoom extends NPCRoom {
 	
 	//This room makes the NPC say different things to the player
+	private boolean active;
+	private NPC presentNPC;
 
 	public MimiRoom(String description) {
 		super(description);
 	}
 	
-	public void speak() {
+	public boolean isActive() {
+		return active;
+	}
+	
+	public String validKeys() {
+		return "wdsak";
+	}
+	
+	public String getContents() {
+		return "j";
+	}
+	public void performAction(int direction) {
+		if(direction == 4) {
+		 if(containsNPC() && presentNPC.isActive) {
+			 presentNPC.interact();
+		 }
+		 else {
+			 CaveExplorer.print("There is nothing to interact with");
+		 }
+		}
+		else {
+		System.out.println("That key does nothing");
+		}
+	}
+	
+	
+	public String getDescription() {
+		if(containsNPC()&& !presentNPC.isActive) {
+		return super.getDescription()+"\n"+presentNPC.getInactiveDescription();
+		}
+		else {
+			String npcDescription="";
+			if(presentNPC!=null) {
+				npcDescription = presentNPC.getActiveDescription();
+			}
+			return super.getDescription()+"\n"+npcDescription;
+		}
+	}
+	
+	public void interact() {
 		CaveExplorer.print("You'll have to go through me if you want Princess Peach!");
-		tauntPlayer();
+		if(isActive() == true) {
+			tauntPlayer();
+		}
 	}
 
 	public String tauntPlayer() {
