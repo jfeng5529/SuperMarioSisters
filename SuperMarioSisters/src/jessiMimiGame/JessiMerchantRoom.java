80f47cfbdc2bc92dea15305c753e619cd59ceb67
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-11-06 10:32:03 -0500

    MerchantRoom

--- SuperMarioSisters/src/jessiMimiGame/JessiMerchantRoom.java
+++ SuperMarioSisters/src/jessiMimiGame/JessiMerchantRoom.java
@@ -1,5 +1,6 @@
 package jessiMimiGame;
 
+import caveExplore.CaveExplorer;
 import caveExplore.CaveRoom;
 
 public class JessiMerchantRoom extends CaveRoom {
@@ -14,8 +15,25 @@ public class JessiMerchantRoom extends CaveRoom {
 	
 	
 	public void purchaseItem() {
-		System.out.println("What would you like to buy?");{
-			
+		System.out.println("What would you like to buy?");
+	}
+	
+	public void performAction(int direction) {
+		if(direction == 0) {
+			CaveExplorer.currentRoom=CaveExplorer.caves[2][4];
+			CaveExplorer.currentRoom.enter();
+		}
+		if(direction == 1) {
+			CaveExplorer.currentRoom=CaveExplorer.caves[1][5];
+			CaveExplorer.currentRoom.enter();
+		}
+		if(direction == 2) {
+			CaveExplorer.currentRoom=CaveExplorer.caves[2][6];
+			CaveExplorer.currentRoom.enter();
+		}
+		if(direction == 3) {
+			CaveExplorer.currentRoom=CaveExplorer.caves[3][5];
+			CaveExplorer.currentRoom.enter();
 		}
 	}
 }
