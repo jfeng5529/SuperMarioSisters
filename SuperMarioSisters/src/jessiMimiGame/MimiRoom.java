65e1dfdf92f45c7d31e0c41b1e3aaa9df0b82e96
mimibancod-buan <mimibancod-buan@mimis-mbp.fios-router.home>
2017-11-06 00:13:21 -0500

    added taunts

--- SuperMarioSisters/src/jessiMimiGame/MimiRoom.java
+++ SuperMarioSisters/src/jessiMimiGame/MimiRoom.java
@@ -1,5 +1,6 @@
 package jessiMimiGame;
 
+import java.util.Random;
 import caveExplore.CaveExplorer;
 import caveExplore.NPCRoom;
 
@@ -13,12 +14,25 @@ public class MimiRoom extends NPCRoom {
 	
 	public void speak() {
 		CaveExplorer.print("You'll have to go through me if you want Princess Peach!");
-		while(isActive) {
-			taunt();
-		}
+		tauntPlayer();
 	}
 
-	public String taunt() {
-		return null;
+	public String tauntPlayer() {
+		Random rand = new Random(); 
+		int value = rand.nextInt(5);
+		
+		String taunt = null;
+		if(value == 0)
+			taunt = "RAAAAAWWWWRRRR!!!!!!";
+		if(value == 1)
+			taunt = "WHAA--?! IT'S YOU! That's it, Mario! I'm gonna stomp you into space bits!";
+		if(value == 2)
+			taunt = "Gah! Give it up already!";
+		if(value == 3)
+			taunt = "Why don't you run off and find some green pipe to hide in?";
+		if(value == 4)
+			taunt = "Go ahead--just try to grab me by the tail! You'll never be able to swing ME around!";
+		
+		return taunt;
 	}
 }
