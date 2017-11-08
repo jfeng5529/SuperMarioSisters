8e6d41ca756fbaf5dfa16ed7fdab59116a75c185
mimibancod-buan <mimibancod-buan@mimis-mbp.fios-router.home>
2017-11-07 22:02:22 -0500

    fix mimi room

--- SuperMarioSisters/src/jessiMimiGame/MimiRoom.java
+++ SuperMarioSisters/src/jessiMimiGame/MimiRoom.java
@@ -7,7 +7,7 @@ import caveExplore.NPCRoom;
 
 public class MimiRoom extends NPCRoom {
 	
-	//This room makes the NPC say different things to the player
+	//This room makes the NPC say different things to the player 
 
 	public MimiRoom(String description) {
 		super(description);
