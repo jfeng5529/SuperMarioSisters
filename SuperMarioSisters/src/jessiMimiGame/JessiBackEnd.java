5932ca9e2fb295c4e2f695af9aa75e16a80cc562
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-11-15 11:40:41 -0500

    ...

--- SuperMarioSisters/src/jessiMimiGame/JessiBackEnd.java
+++ SuperMarioSisters/src/jessiMimiGame/JessiBackEnd.java
@@ -1,24 +1,61 @@
 package jessiMimiGame;
 
+import caveExplorer.JessiMimiPlot;
+
 public class JessiBackEnd implements MimiSupporter{
 	private JessiSupporter frontend;
 	private JessiMimiPlot[][] plots;
 	private int score;
+	private int numberOfMines;
+	private boolean safeSpot;
 
-public JessiBackEnd(JessiSupporter frontend) {
-	this.frontend = frontend;
-	plots = new JessiMimiPlot[8][8];
-	numberOfMines = 6;
-	createPlots();
-}
+	public JessiBackEnd(JessiSupporter frontend) {
+		this.frontend = frontend;
+		plots = new JessiMimiPlot[8][8];
+		numberOfMines = 6;
+		createPlots();
+	}
 
-private void createPlots() {
-	// TODO Auto-generated method stub
+	public void createPlots() {
+		for(int row = 0; row < plots.length; row++){
+			for(int col = 0; col < plots[row].length; col++){
+				plots[row][col] = new JessiMimiPlot(row, col);
+			}
+		}
+	}
 	
-}
 
-private boolean checkWin() {
-	return false;
-}
 
+	@Override
+	public JessiMimiPlot[][] getPlots() {
+		return plots;
+
+	}	
+
+	@Override
+	public boolean checkForMine() {
+		if(!safeSpot) {
+		return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean checkWin() {
+		// TODO Auto-generated method stub
+		return false;
+	}	
+
+	public void gameOver() {
+		if(checkForMine() == true) {
+			MimiFrontEnd.startGame();
+		}
+		if(checkWin() == true) {
+			System.out.println("Wahh! Nooo! You have defeated me!");
+			//Mini game over proceeds to the next room
+		}
+		//else {
+		//	MimiFrontEnd.displayScoreStatus();
+		//}
+	}
 }
--- SuperMarioSisters/src/jessiMimiGame/JessiMimiPlot.java
+++ SuperMarioSisters/src/jessiMimiGame/JessiMimiPlot.java
@@ -1,9 +1,22 @@
 package jessiMimiGame;
 
-import java.util.Scanner;
-
 public class JessiMimiPlot {
 	
+	public JessiMimiPlot(int row2, int col) {
+		// TODO Auto-generated constructor stub
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
 	private int row;
 	private int column;
 
