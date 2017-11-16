23761d152b72e51aff032b696c3c619be2d1a729
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-11-16 11:43:01 -0500

    did things

--- SuperMarioSisters/src/jessiMimiGame/MimiFrontEnd.java
+++ SuperMarioSisters/src/jessiMimiGame/MimiFrontEnd.java
@@ -27,13 +27,17 @@ public class MimiFrontEnd implements JessiSupporter{
 	}
 
 	public void menu() {
+		System.out.println("Press 'r' for the rules of my game or press 's' to start, but it won't be easy!");
 		rules();
 		startGame();
-		System.out.println("Enter 'i' for instructions on how to play or 's' to start playing!");
 	}
 	
-	private void rules() {
-		
+	private String rules() {
+		return "MWAHAHAHAAA! This is Bob-omb Sweeper! Try not to choose a space with Bob-ombs hidden in them."
+				+ "\n Enter in the coordinates of a space on my map, and some empty spaces will open up."
+				+ "\n Each time an empty space is revealed, it will show the number of spaces in its perimeter that have Bob-ombs hidden in them!"
+				+ "\n Each time you successfully choose an empty space, you earn five points!"
+				+ "\n If you can get 50 points, you have defeated me, but I'm not an easy guy to beat! Try me!";
 	}
 	
 	public static void startGame() {
@@ -61,7 +65,7 @@ public class MimiFrontEnd implements JessiSupporter{
 	
 	public static void displayScoreStatus() {
 		String scoreStatus = "";
-		System.out.println("Your have a current score of " + score + " points!");
+		System.out.println("You have a current score of " + score + " points!");
 	}
 	
 }
