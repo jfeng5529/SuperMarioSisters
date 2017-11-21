f6d0c7147744137d86ae61c70d39b7dec77a0947
Jessz5 <31857213+Jessz5@users.noreply.github.com>
2017-11-21 11:45:16 -0500

    Update Mines.java

--- SuperMarioSisters/src/jessiMimiGame/Mines.java
+++ SuperMarioSisters/src/jessiMimiGame/Mines.java
@@ -27,10 +27,22 @@ public class Mines extends JessiBackEnd {
 		for (int row = 0; row < plots.length; row++) {
 			 for (int col = 0; col < plots.length; col++) {
 				 if (!(plots[row][col].getIsMine())) {
-					 
+					 int count = 0;
+					 for (int i = row - 1; i <= row + 1; i++) {
+	                    for (int j = col - 1; j <= col + 1; j++) {
+	                    	 if (0 <= i && i < plots.length && 0 <= j && j < plots.length) {
+	                                 if (plots[row][col].getIsMine()) {
+	                                     count++;
+	                                 }
+	                    	 }
+	                    }
+					 }
+					  plots[row][col].setAdjacentMines(count);
 				 }
-					 
 			 }
 		}
 	}
+	
+	public void 
+
 }
