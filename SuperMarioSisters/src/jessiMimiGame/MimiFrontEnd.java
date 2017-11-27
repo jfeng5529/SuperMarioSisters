84dd46aeac6bb09dcb4b12f51d1151d37989eba7
mimibancod-buan <mimibancod-buan@mimis-mbp.fios-router.home>
2017-11-26 23:20:37 -0500

    fixes

--- SuperMarioSisters/src/jessiMimiGame/MimiFrontEnd.java
+++ SuperMarioSisters/src/jessiMimiGame/MimiFrontEnd.java
@@ -1,124 +1,87 @@
 package jessiMimiGame;
 
+import java.util.Scanner;
+
 import caveExplore.CaveExplorer;
 import caveExplore.CaveRoom;
 import caveExplore.NPCRoom;
 
 public class MimiFrontEnd implements JessiSupporter{
-	
 	private static MimiSupporter backend;
-	private static int score;
-	private static boolean safeSpot;
-	private int moves;
-	
-	private int[][] mines;
+	private boolean safeSpot = true;
+    private boolean isWin;
+    private boolean cheat;
 
+	public MimiFrontEnd() {
+		backend = new JessiBackEnd(this);
+		CaveExplorer.in = new Scanner(System.in);
+		isWin = false;
+		cheat = false;
+	}
+	
 	public static final void main(String[] args) {
 		MimiFrontEnd demo = new MimiFrontEnd();
 		demo.play();
 	}
 
-	public MimiFrontEnd() {
-		backend = new JessiBackEnd(this);
-		moves = 0;
-	}
-
-	public void play() {
+	private void play() {
 		introduction();
-		menu();
+		//startGame();
 	}
-
-	private String introduction() {
-		return "AHAHAHAHA!!!! You've made it this far, but can you defeat me??? BOWSER????";
-	}
-
-	public void menu() {
-		System.out.println("BOSS BATTLE! Press 'r' for the rules of my game or press 'p' to play, but it won't be easy!");
-		String command = MimiUtility.waitForLetterInput("rp");
-		if(command.equals("r")){
-			MimiUtility.print("MWAHAHAHAAA! This is Bob-omb Sweeper! Try not to choose a space with Bob-ombs hidden in them."
-					+ "\n Enter in the coordinates of a space on my map, and some empty spaces will open up."
-					+ "\n Each time an empty space is revealed, it will show the number of spaces in its perimeter that have Bob-ombs hidden in them!"
-					+ "\n Each time you successfully choose an empty space, you earn five points!"
-					+ "\n If you can get 50 points, you have defeated me, but I'm not an easy guy to beat! Try me!");
-			CaveExplorer.in.nextLine();
-			menu();
-		}else{
-			startGame();
+	
+	private void startGame() {
+		JessiMineField plots = backend.getPlots();
+		while(safeSpot) {
+			JessiBackEnd.displayField(plots);
+			System.out.println("Type in the row and column!");
+			int[] coords = backend.getCoordInput();
+			if(coords[2] == 1) {
+				cheat = true;
+				break;
+			}
+			if (backend.reveal(coords[0], coords[1])) {
+				safeSpot = false;
+				JessiBackEnd.displayField(plots);
+			}
+			if(backend.isWin()) {
+				System.out.println("Ahhh YOU WIN!! FINE! Take your prince!!");
+				//JessiBackEnd.displayField(plots);
+			}
+		}
+		if (cheat) {
+			System.out.println("Too Hard?  Here's the answer!");
+			plots.setRevealAll();
+			JessiBackEnd.displayField(plots);
+		}
+		else {
+			System.out.println("                   YOU HAVE HIT A BOB-OMB!!!");
+			System.out.println("                 __________Game Over__________\n");
 		}
 	}
-
+		
 	public String validKeys() {
 		return "wasdrp";
 	}
-
-	private String rules() {
-		return "MWAHAHAHAAA! This is Bob-omb Sweeper! Try not to choose a space with Bob-ombs hidden in them."
-		+ "\n Enter in the coordinates of a space on my map, and some empty spaces will open up."
-		+ "\n Each time an empty space is revealed, it will show the number of spaces in its perimeter that have Bob-ombs hidden in them!"
-		+ "\n Each time you successfully choose an empty space, you earn five points!"
-		+ "\n If you can get 50 points, you have defeated me, but I'm not an easy guy to beat! Try me!";
+	
+	private void rules() {
+		MimiUtility.print("MWAHAHAHAAA! This is Bob-omb Sweeper! Try not to choose a space with Bob-ombs hidden in them."
+				+ "\n Enter in the coordinates of a space on my map, and the space will either be empty or a Bob-omb!."
+				+ "\n Each time an empty space is revealed, it will show the number of spaces in its perimeter that have Bob-ombs hidden in them!"
+				+ "\n If you can reveal all of the empty spaces without revealing a Bob-omb, you can take your prince!"
+				+ "\n Sounds easy? I'm not an easy guy to beat! Try me!"); 
 	}
-
-	public static void startGame() {
-		JessiMimiPlot[][] plots = backend.getPlots();
-		JessiMimiPlot p = null;
-		while(safeSpot == true) {
-			score += 5;
-			displayScoreStatus();
-			if(backend.checkWin()) {
-				safeSpot = false;
-			}
-			else {
-				//JessiBackEnd.getValidUserInput();
-				if(backend.checkForMine()) {
-					safeSpot = false;
-					System.out.println("___________Game Over___________\n");
-				}
-			}
+	
+	private void introduction() {
+		System.out.println("BOSS LEVEL:YOU'VE ENCOUNTERED BOWSER, IN ORDER TO RESCUE YOUR PRINCE AND PROCEED TO THE NEXT ROOM WIN THIS GAME.");
+		System.out.println("Press the letter 'r' to get the instructions or 'p' to play.");
+		String command = MimiUtility.waitForLetterInput("rp");
+		if(command.equals("r")) {
+			rules();
+			startGame();
+		}else {
+			startGame();
 		}
+	
 	}
-
-	public boolean allOpenSpacesChecked(){
-		int count=0;
-		for(int row = 1 ; row < 9 ; row++)
-			for(int column = 1 ; column < 9 ; column++)
-				if(JessiMimiPlot[row][column]=='-')
-					count++;
-		if(count == 10)
-			return true;
-		else
-			return false;                
-	}
-
-	public void displayField(JessiMimiPlot[][] plots) {
-		String rows = "0123456789";
-		String columns = "  0123456789";
-		for(int row = 0; row < plots.length; row++){
-			System.out.print(rows.substring(row, row+1)+"-");
-			for(int col = 0; col < plots[row].length; col++){
-				if(plots[row][col].isRevealed()){
-					if(plots[row][col].JessiBackEnd.isMine()){
-						System.out.print("*");
-					}else{
-						System.out.print(" ");	
-					}
-				}
-				System.out.println(" " + rows.substring(row, row+1));
-			}
-			System.out.println(columns.substring(0, plots[0].length+2));
-		}
-		
-		public void startBoard(){
-	        for(int row = 1 ; row < mines.length ; row++)
-	            for(int col = 1 ; col < mines.length ; col++)
-	                JessiMimiPlot[row][col]= '-';
-	    }
-
-
-		public static void displayScoreStatus() {
-			//String scoreStatus = "";
-			System.out.println("You have a current score of " + score + " points!");
-		}
-
-	}
+	
+}
