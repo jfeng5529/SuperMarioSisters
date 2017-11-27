fa8e0df3c86c72902ed003e9a5bb7c3299f18df3
Jessi <Jessi@jessis-mbp.home>
2017-11-26 22:26:49 -0500

    Mushrooms

--- SuperMarioSisters/src/jessiMimiGame/JessiMerchantRoom.java
+++ SuperMarioSisters/src/jessiMimiGame/JessiMerchantRoom.java
@@ -13,22 +13,25 @@ public class JessiMerchantRoom extends NPCRoom {
 	}
 	
 	public void printAllowedEntry() {
-		CaveExplorer.print("Welcome to the Item Shop. Enter w, a, s, d to move or type h to interact.");
+		CaveExplorer.print("Welcome to the Item Shop. I'm Toad the Merchant. Enter w, a, s, d to move");
 	}
 	
 	public String validKeys() {
-		return "wdsah";
+		return "wdsasm";
 	}
 	
 	public void performAction(int direction) {
 		if(direction == 4) {
-			merchantToad.chat();
+			
 			
 		}else {
 			 super.performAction(direction);
 		}
 	}
 	
+	public void response() {
+		System.out.println("You have successfully purchased your item!");
+	}
 	
 }
 
